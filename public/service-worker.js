b74fd106aad0e81a56c9c74489874813e870b255
emccar22 <e.mccarthy2285@gmail.com>
2022-03-13 22:37:12 -0400

    add files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,6 +16,8 @@ const FILES_TO_CACHE = [
   "./icons/icon-192x192.png",
   "./icons/icon-384x384.png",
   "./icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
 self.addEventListener("install", function (e) {
